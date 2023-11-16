1ee501251c5b76edef3ea632d1661ae1cc3cc9ca
xb0yx2k20 <musraul@yandex.ru>
2023-11-16 23:29:51 +0300

    todo

--- App.py
+++ App.py
@@ -1,24 +1,21 @@
-from flask import Flask, render_template, request
-from models import Artist, Album, Song, Car, db
-import logging
-logging.basicConfig()
-logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
+from flask import Flask, render_template, request, redirect, url_for
+from models import Car, db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///music.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# связываем приложение и экземпляр SQLAlchemy
 db.init_app(app)
-'''
-@app.route('/')
-def songs():
-    songs_list = Song.query.all()
-    return render_template('songs.html', songs=songs_list)'''
 
-@app.route('/car')
+@app.route('/cars')
 def cars():
     cars_list = Car.query.all()
+    search_query = request.args.get('search', '')
+    
+    return render_template('cars.html', cars=cars_list)
+
+@app.route('/cars/search', methods=['GET'])
+def cars_search():
     search_query = request.args.get('search', '')
     if search_query:
         cars_list = Car.query.filter(Car.Mark.ilike(f"%{search_query}%")).all()
---
+++ README.md
@@ -0,0 +1,5 @@
+# To Do
+
+* Ajax + js search in real time
+* images
+* delete auto scrolling caorusel
